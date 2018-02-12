ff2e835762f69b5842f9dd84569e201ba86d3acd
Timofey Mukha <timofey.mukha@it.uu.se>
2018-02-12 13:09:51 +0100

    inits

--- wallriori/__init__.py
+++ wallriori/__init__.py
@@ -4,6 +4,14 @@
 # See LICENCE.txt and the Legal section in the README for more information
 
 from .rootfinders import *
+from .lawsofthewall import *
+from .mesh import *
+from .wallmodels import *
+from .problems import *
 
-__all__ = ["rootfinders"]
+__all__ = ["rootfinders", "lawsofthewall", "mesh", "wallmodels", "problems"]
 __all__.extend(rootfinders.__all__)
+__all__.extend(lawsofthewall.__all__)
+__all__.extend(mesh.__all__)
+__all__.extend(wallmodels.__all__)
+__all__.extend(problems.__all__)
---
+++ wallriori/mesh/__init__.py
@@ -0,0 +1,9 @@
+# This file is part of wallriori
+# (c) Timofey Mukha
+# The code is released under the MIT Licence.
+# See LICENCE.txt and the Legal section in the README for more information
+
+from .mesh import *
+
+__all__ = ["mesh"]
+__all__.extend(mesh.__all__)
