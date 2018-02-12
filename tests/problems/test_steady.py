25c7185194ab11668f72bfd3cc61e0aacb0c9ff6
Timofey Mukha <timofey.mukha@it.uu.se>
2018-02-12 13:08:32 +0100

    More tests

--- tests/problems/test_steady.py
+++ tests/problems/test_steady.py
@@ -7,42 +7,26 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from wallriori.mesh import Mesh
+from wallriori.problems import SteadyDiffusion
 import numpy as np
 from numpy.testing import assert_array_equal
+from numpy.testing import assert_allclose
 
 
 def test_mesh_init():
-    m = Mesh.from_faces(np.linspace(0, 1, 11))
-    assert_array_equal(m.faces, np.linspace(0, 1, 11))
+    mesh = Mesh.from_faces(np.linspace(0, 1, 11))
+    nu = np.ones(mesh.centres.size)
+
+    ic = np.zeros(mesh.centres.size)
+    problem = SteadyDiffusion(mesh, ic, nu, [100, 2], [10, 2], 100)
 
 
-def test_mesh_mutable():
-    m = Mesh.from_faces(np.linspace(0, 1, 11))
-    m.nCells = 12
-    m.dim2 = 12
-    m.dim3 = 12
-    assert m.nCells == 12
-    assert m.dim2 == 12
-    assert m.dim3 == 12
+def test_solve_linear():
+    mesh = Mesh.from_faces(np.linspace(0, 1, 11))
+    nu = np.ones(mesh.centres.size)
+    ic = np.zeros(mesh.centres.size)
+    source = 0
+    problem = SteadyDiffusion(mesh, ic, nu, [0, 1], [1, 1], source)
+    problem.solve()
 
-
-def test_mesh_volumes():
-    m = Mesh.from_faces(np.linspace(0, 3, 4))
-    volumes = np.array([1, 1, 1])
-    assert_array_equal(m.volumes, volumes)
-
-
-def test_mesh_centres():
-    m = Mesh.from_faces(np.linspace(0, 1, 3))
-    centres = np.array([0.25, 0.75])
-    assert_array_equal(m.centres, centres)
-
-
-def test_mesh_from_centres_two_cells():
-    m = Mesh.from_centres_and_edges(np.array([0, 0.25, 0.75, 1]))
-    assert_array_equal(m.faces, np.array([0, 0.5, 1]))
-
-
-def test_mesh_from_centres_one_cell():
-    m = Mesh.from_centres_and_edges(np.array([0, 0.5, 1]))
-    assert_array_equal(m.faces, np.array([0, 1]))
+    assert_allclose(problem.solution, mesh.centres, atol=1e-15)
